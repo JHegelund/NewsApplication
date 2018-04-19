99ebc683e77222902379dfe12b4aaae2144d656c
JHegelund <hegelund.jacob@gmail.com>
2018-04-19 22:49:24 +0200

    client finished

--- clientserver/src/client.cc
+++ clientserver/src/client.cc
@@ -13,41 +13,20 @@
 
 using namespace std;
 
-/*
- * Send an integer to the server as four bytes.
- */
-void writeNumber(const Connection& conn, int value) {
-	conn.write((value >> 24) & 0xFF);
-	conn.write((value >> 16) & 0xFF);
-	conn.write((value >> 8)	 & 0xFF);
-	conn.write(value & 0xFF);
-}
-
-/*
- * Read a string from the server.
- */
-string readString(const Connection& conn) {
-	string s;
-	char ch;
-	while ((ch = conn.read()) != '$') {
-		s += ch;
-	}
-	return s;
-}
 
 /*
  * Writes and receives list newsgroups command to and from server
  */
-void listNG(const Connection& conn){
-	MessageHandler::sendCode(conn, static_cast<size_t>(Protocol::COM_LIST_NG));
-	MessageHandler::sendCode(conn, static_cast<size_t>(Protocol::COM_END));
+void listNG(MessageHandler& mh){
+	mh.sendCode(Protocol::COM_LIST_NG);
+	mh.sendCode(Protocol::COM_END);
 
 	vector<pair<int, string>> ngs;
-	if(MessageHandler::recvCode(conn) == static_cast<int>(Protocol::ANS_LIST_NG)){
-		int num = MessageHandler::recvIntParameter(conn);
+	if(mh.recvCode() == Protocol::ANS_LIST_NG){
+		int num = mh.recvIntParameter();
 		for(int i = 0; i < num; i++){
-			int id = MessageHandler::recvIntParameter(conn);
-			string name = MessageHandler::recvStringParameter(conn);
+			int id = mh.recvIntParameter();
+			string name = mh.recvStringParameter();
 			//ngs[i].first = id;
 			//ngs[i].second = name;
 			ngs.push_back(make_pair(id, name));
@@ -59,7 +38,7 @@ void listNG(const Connection& conn){
 	/*
 	 * Output newsgroups to client
 	 */
-	if(MessageHandler::recvCode(conn) != static_cast<int>(Protocol::ANS_END)){
+	if(mh.recvCode() != Protocol::ANS_END){
 		cout << "Protocol Violation" << endl;
 	} else {
 		for(pair<int, string>& ng : ngs){
@@ -71,28 +50,28 @@ void listNG(const Connection& conn){
 /*
  * List articles within specified newgroup
  */
-void listArticles(const Connection& conn, int ngID){
-	MessageHandler::sendCode(conn, static_cast<size_t>(Protocol::COM_LIST_ART));
-	MessageHandler::sendIntParameter(conn, ngID);
-	MessageHandler::sendCode(conn, static_cast<size_t>(Protocol::COM_END));
+void listArticles(MessageHandler& mh, int ngID){
+	mh.sendCode(Protocol::COM_LIST_ART);
+	mh.sendIntParameter(ngID);
+	mh.sendCode(Protocol::COM_END);
 
 	vector<pair<int, string>> arts;
-	if(MessageHandler::recvCode(conn) == static_cast<int>(Protocol::ANS_LIST_ART)){
-		int ans = MessageHandler::recvCode(conn);
-		if(ans == static_cast<int>(Protocol::ANS_ACK)){
-			int num = MessageHandler::recvIntParameter(conn);
+	if(mh.recvCode() == Protocol::ANS_LIST_ART){
+		Protocol ans = mh.recvCode();
+		if(ans == Protocol::ANS_ACK){
+			int num = mh.recvIntParameter();
 			for(int i = 0; i < num; i++){
-				int id = MessageHandler::recvIntParameter(conn);
-				string name = MessageHandler::recvStringParameter(conn);
+				int id = mh.recvIntParameter();
+				string name = mh.recvStringParameter();
 				arts.push_back(make_pair(id, name));
 			}
 		} else if(ans == static_cast<int>(Protocol::ANS_NAK)){
-			if(MessageHandler::recvCode(conn) == static_cast<int>(Protocol::ERR_NG_DOES_NOT_EXIST)){
+			if(mh.recvCode() == Protocol::ERR_NG_DOES_NOT_EXIST){
 				cout << "Error newsgroup does not exist" << endl;
 			}
 		}
 		
-		if(MessageHandler::recvCode(conn) != static_cast<int>(Protocol::ANS_END)){
+		if(mh.recvCode() != Protocol::ANS_END){
 			cout << "Protocol Violation" << endl;
 		} else {
 			for(pair<int, string>& a : arts){
@@ -105,23 +84,23 @@ void listArticles(const Connection& conn, int ngID){
 /*
  * Create newgroup with specified name
  */
-void createNG(const Connection& conn, string name){
-	MessageHandler::sendCode(conn, static_cast<size_t>(Protocol::COM_CREATE_NG));
-	MessageHandler::sendStringParameter(conn, name);
-	MessageHandler::sendCode(conn, static_cast<size_t>(Protocol::COM_END));
+void createNG(MessageHandler& mh, string name){
+	mh.sendCode(Protocol::COM_CREATE_NG);
+	mh.sendStringParameter(name);
+	mh.sendCode(Protocol::COM_END);
 
-	if(MessageHandler::recvCode(conn) == static_cast<int>(Protocol::ANS_CREATE_NG)){
-		int ans = MessageHandler::recvCode(conn);
-		if(ans == static_cast<int>(Protocol::ANS_ACK)){
-			if(MessageHandler::recvCode(conn) == static_cast<int>(Protocol::ANS_END)){
+	if(mh.recvCode() == Protocol::ANS_CREATE_NG){
+		Protocol ans = mh.recvCode();
+		if(ans == Protocol::ANS_ACK){
+			if(mh.recvCode() == Protocol::ANS_END){
 				cout << "Article added" << endl;
 			} else {
 				cout << "Protocol Violation" << endl;
 			}
-		} else if(ans == static_cast<int>(Protocol::ANS_NAK)){
-			if(MessageHandler::recvCode(conn) == static_cast<int>(Protocol::ERR_NG_ALREADY_EXISTS)){
+		} else if(ans == Protocol::ANS_NAK){
+			if(mh.recvCode() == Protocol::ERR_NG_ALREADY_EXISTS){
 				cout << "Newgroup already exist" << endl;
-				if(MessageHandler::recvCode(conn) != static_cast<int>(Protocol::ANS_END)){
+				if(mh.recvCode() != Protocol::ANS_END){
 					cout << "Protocol Violation" << endl;
 				}
 			} else {
@@ -131,28 +110,28 @@ void createNG(const Connection& conn, string name){
 	}
 }
 
-void deleteNG(const Connection& conn, int id){
-	MessageHandler::sendCode(conn, static_cast<size_t>(Protocol::COM_DELETE_NG));
-	MessageHandler::sendIntParameter(conn, id);
-	MessageHandler::sendCode(conn, static_cast<size_t>(Protocol::COM_END));
+void deleteNG(MessageHandler& mh, int id){
+	mh.sendCode(Protocol::COM_DELETE_NG);
+	mh.sendIntParameter(id);
+	mh.sendCode(Protocol::COM_END);
 
-	if(MessageHandler::recvCode(conn) == static_cast<int>(Protocol::ANS_DELETE_NG)){
-		int ans = MessageHandler::recvCode(conn);
-		if(ans == static_cast<int>(Protocol::ANS_ACK)){
-			if(MessageHandler::recvCode(conn) == static_cast<int>(Protocol::ANS_END)){
+	if(mh.recvCode() == Protocol::ANS_DELETE_NG){
+		Protocol ans = mh.recvCode();
+		if(ans == Protocol::ANS_ACK){
+			if(mh.recvCode() == Protocol::ANS_END){
 				cout << "Newsgroup deleted" << endl;
 			} else {
 				cout << "Protocol Violation" << endl;
 			}
-		} else if (ans == static_cast<int>(Protocol::ANS_NAK)){
-			if(MessageHandler::recvCode(conn) == static_cast<int>(Protocol::ERR_NG_DOES_NOT_EXIST)){
+		} else if (ans == Protocol::ANS_NAK){
+			if(mh.recvCode() == Protocol::ERR_NG_DOES_NOT_EXIST){
 				cout << "Newsgroup does not exist" << endl;
-				if(MessageHandler::recvCode(conn) != static_cast<int>(Protocol::ANS_END)){
+				if(mh.recvCode() != Protocol::ANS_END){
 					cout << "Protocol Violation" << endl;
 				}
 			} else {
 				cout << "Protocol Violation" << endl;
-				if(MessageHandler::recvCode(conn) != static_cast<int>(Protocol::ANS_END)){
+				if(mh.recvCode() != Protocol::ANS_END){
 					cout << "Protocol Violation" << endl;
 				}
 			}
@@ -162,28 +141,28 @@ void deleteNG(const Connection& conn, int id){
 	}
 }
 
-void deleteArt(const Connection& conn, int ngID, int artID){
-	MessageHandler::sendCode(conn, static_cast<size_t>(Protocol::COM_DELETE_ART));
-	MessageHandler::sendIntParameter(conn, ngID);
-	MessageHandler::sendIntParameter(conn, artID);
-	MessageHandler::sendCode(conn, static_cast<size_t>(Protocol::COM_END));
+void deleteArt(MessageHandler& mh, int ngID, int artID){
+	mh.sendCode(Protocol::COM_DELETE_ART);
+	mh.sendIntParameter(ngID);
+	mh.sendIntParameter(artID);
+	mh.sendCode(Protocol::COM_END);
 
-	if(MessageHandler::recvCode(conn) == static_cast<int>(Protocol::ANS_DELETE_ART)){
-		int ans = MessageHandler::recvCode(conn);
-		if(ans == static_cast<int>(Protocol::ANS_ACK)){
-			if(MessageHandler::recvCode(conn) == static_cast<int>(Protocol::ANS_END)){
+	if(mh.recvCode() == Protocol::ANS_DELETE_ART){
+		Protocol ans = mh.recvCode();
+		if(ans == Protocol::ANS_ACK){
+			if(mh.recvCode() == Protocol::ANS_END){
 				cout << "Article deleted" << endl;
 			}
-		} else if(ans == static_cast<int>(Protocol::ANS_NAK)){
-			int err = MessageHandler::recvCode(conn);
-			if(err == static_cast<int>(Protocol::ERR_NG_DOES_NOT_EXIST)){
+		} else if(ans == Protocol::ANS_NAK){
+			Protocol err = mh.recvCode();
+			if(err == Protocol::ERR_NG_DOES_NOT_EXIST){
 				cout << "Newsgroup does not exist" << endl;
-				if(MessageHandler::recvCode(conn) != static_cast<int>(Protocol::ANS_END)){
+				if(mh.recvCode() != Protocol::ANS_END){
 					cout << "Protocol Violation" << endl;
 				}
-			} else if(err == static_cast<int>(Protocol::ERR_ART_DOES_NOT_EXIST)){
+			} else if(err == Protocol::ERR_ART_DOES_NOT_EXIST){
 				cout << "Article does not exist" << endl;
-				if(MessageHandler::recvCode(conn) != static_cast<int>(Protocol::ANS_END)){
+				if(mh.recvCode() != Protocol::ANS_END){
 					cout << "Protocol Violation" << endl;
 				}
 			}
@@ -193,53 +172,53 @@ void deleteArt(const Connection& conn, int ngID, int artID){
 	}
 }
 
-void readArt(const Connection& conn, int ngID, int artID){
-	MessageHandler::sendCode(conn, static_cast<size_t>(Protocol::COM_GET_ART));
-	MessageHandler::sendIntParameter(conn, ngID);
-	MessageHandler::sendIntParameter(conn, artID);
-	MessageHandler::sendCode(conn, static_cast<size_t>(Protocol::COM_END));
+void readArt(MessageHandler& mh, int ngID, int artID){
+	mh.sendCode(Protocol::COM_GET_ART);
+	mh.sendIntParameter(ngID);
+	mh.sendIntParameter(artID);
+	mh.sendCode(Protocol::COM_END);
 
 	string author = "";
 	string title = "";
 	string text = "";
 
-	if(MessageHandler::recvCode(conn) == static_cast<int>(Protocol::ANS_GET_ART)){
-		int ans = MessageHandler::recvCode(conn);
-		if(ans == static_cast<int>(Protocol::ANS_ACK)){
-			string author = MessageHandler::recvStringParameter(conn);
-			string title = MessageHandler::recvStringParameter(conn);
-			string text = MessageHandler::recvStringParameter(conn);
-			if(MessageHandler::recvCode(conn) == static_cast<int>(Protocol::ANS_END)){
+	if(mh.recvCode() == Protocol::ANS_GET_ART){
+		Protocol ans = mh.recvCode();
+		if(ans == Protocol::ANS_ACK){
+			string author = mh.recvStringParameter();
+			string title = mh.recvStringParameter();
+			string text = mh.recvStringParameter();
+			if(mh.recvCode() == Protocol::ANS_END){
 				cout << "Author: " << author << "\n" << "Title: " << title << "\n" << text << endl;
 			}
-		} else if(ans == static_cast<int>(Protocol::ANS_NAK)){
-			int nak = MessageHandler::recvCode(conn);
-			if(nak == static_cast<int>(Protocol::ERR_NG_DOES_NOT_EXIST)){
+		} else if(ans == Protocol::ANS_NAK){
+			Protocol nak = mh.recvCode();
+			if(nak == Protocol::ERR_NG_DOES_NOT_EXIST){
 				cout << "Newsgroup does not exist" << endl;
-			} else if(nak == static_cast<int>(Protocol::ERR_ART_DOES_NOT_EXIST)){
+			} else if(nak == Protocol::ERR_ART_DOES_NOT_EXIST){
 				cout << "Article does not exist" << endl;
 			}
-			if(MessageHandler::recvCode(conn) != static_cast<int>(Protocol::ANS_END)){
+			if(mh.recvCode() != Protocol::ANS_END){
 				cout << "Protocol Violation" << endl;
 			}
 		}
 	}
 }
 
-void createArt(const Connection& conn, int ngID, string title, string author, string text){
-	MessageHandler::sendCode(conn, static_cast<size_t>(Protocol::COM_GET_ART));
-	MessageHandler::sendIntParameter(conn, ngID);
-	MessageHandler::sendStringParameter(conn, title);
-	MessageHandler::sendStringParameter(conn, author);
-	MessageHandler::sendStringParameter(conn, text);
-	MessageHandler::sendCode(conn, static_cast<size_t>(Protocol::COM_END));
+void createArt(MessageHandler& mh, int ngID, string title, string author, string text){
+	mh.sendCode(Protocol::COM_GET_ART);
+	mh.sendIntParameter(ngID);
+	mh.sendStringParameter(title);
+	mh.sendStringParameter(author);
+	mh.sendStringParameter(text);
+	mh.sendCode(Protocol::COM_END);
 
-	if(MessageHandler::recvCode(conn) == static_cast<int>(Protocol::ANS_CREATE_ART)){
-		int ans = MessageHandler::recvCode(conn);
-		if(ans == static_cast<int>(Protocol::ANS_ACK)){
+	if(mh.recvCode() == Protocol::ANS_CREATE_ART){
+		Protocol ans = mh.recvCode();
+		if(ans == Protocol::ANS_ACK){
 			cout << "Article created" << endl;
-		} else if (ans == static_cast<int>(Protocol::ANS_NAK)){
-			if(MessageHandler::recvCode(conn) == static_cast<int>(Protocol::ERR_NG_DOES_NOT_EXIST)){
+		} else if (ans == Protocol::ANS_NAK){
+			if(mh.recvCode() == Protocol::ERR_NG_DOES_NOT_EXIST){
 				cout << "Error, newgroup does not exist" << endl;
 			} else {
 				cout << "Protocol Violation" << endl;
@@ -247,7 +226,7 @@ void createArt(const Connection& conn, int ngID, string title, string author, st
 		} else {
 			cout << "Protocol Violation" << endl;
 		}
-		if(MessageHandler::recvCode(conn) != static_cast<int>(Protocol::ANS_END)){
+		if(mh.recvCode() != Protocol::ANS_END){
 			cout << "Protocol Violation" << endl;
 		}
 	} else {
@@ -312,6 +291,7 @@ int main(int argc, char* argv[]) {
 
 	cout << "Welcome! Use any of the commands below to use this application." << endl;
 	listCommands();
+	MessageHandler mh(conn);
 	string cmd;
 	while (cin >> cmd) {
 		try {
@@ -319,21 +299,21 @@ int main(int argc, char* argv[]) {
 				cin >> cmd;
 				if(cmd == "newsgroups"){
 					cout << "Listing newgroups...." << endl;
-					listNG(conn);
+					listNG(mh);
 				} else if(cmd == "articles"){
 					//Read newsgroup id
 					cin >> cmd;
 					int ngID = readNgId(cmd);
 					if(ngID != 0){
 						//List articles within this newsgroup
-						listArticles(conn, ngID);
+						listArticles(mh, ngID);
 					}
 				}
 			} else if (cmd == "Create"){
 				cin >> cmd;
 				if(cmd == "newsgroup"){
 					cin >> cmd;
-					createNG(conn, cmd);
+					createNG(mh, cmd);
 				}
 			} else if(cmd == "Delete"){
 				cin >> cmd;
@@ -341,7 +321,7 @@ int main(int argc, char* argv[]) {
 					cin >> cmd;
 					int ngID = readNgId(cmd);
 					if(ngID != 0){
-						deleteNG(conn, ngID);
+						deleteNG(mh, ngID);
 					}
 				} else if (cmd == "article"){
 					cin >> cmd;
@@ -350,7 +330,7 @@ int main(int argc, char* argv[]) {
 						cin >> cmd;
 						int artID = readArtId(cmd);
 						if(artID != 0){
-							deleteArt(conn, ngID, artID);
+							deleteArt(mh, ngID, artID);
 						}
 					}
 				}
@@ -361,7 +341,7 @@ int main(int argc, char* argv[]) {
 					cin >> cmd;
 					int artID = readArtId(cmd);
 					if(artID != 0){
-						readArt(conn, ngID, artID);
+						readArt(mh, ngID, artID);
 					}
 				}
 			} else if(cmd == "Write"){
@@ -379,7 +359,7 @@ int main(int argc, char* argv[]) {
 					cout << "Enter text" << endl;
 					string text;
 					getline(cin, text);
-					createArt(conn, ngID, title, author, text);
+					createArt(mh, ngID, title, author, text);
 				}
 			} else if(cmd == "Help"){
 				listCommands();
